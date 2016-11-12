5ff25c9a22076dea5126ac18db3a4d2f749aa844
Kay Cha <kay@hannal.net>
2016-11-12 11:26:18 +0900

    Add toppage 뷰 함수와 url 연결

--- trumpquiz/urls.py
+++ trumpquiz/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 
 from quiz.views import helloworld
 from quiz.views import helloworld2
+from quiz.views import toppage
 
 
 urlpatterns = [
+    url(r'^$', toppage),
     url(r'^hello2/$', helloworld2),
     url(r'^hello/$', helloworld),
     url(r'^admin/', admin.site.urls),
