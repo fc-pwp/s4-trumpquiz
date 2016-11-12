e70b0aece3c9e4a679e560a1b9da9e3f2bcf078a
Kay Cha <kay@hannal.net>
2016-11-12 11:45:58 +0900

    Add quizzes 항목들 추가

--- quiz/views.py
+++ quiz/views.py
@@ -12,7 +12,27 @@ def helloworld2(request):
 
 
 def toppage(request):
+    quizzes = [
+        {
+            'image_url': 'http://images.freeimages.com/images/previews/3e2/weather-vane-1424027.jpg',
+            'quiz_title': '첫 번째 퀴즈',
+            'quiz_description': '우앙 재밌는 퀴즈',
+        },
+        {
+            'image_url': 'http://images.freeimages.com/images/previews/3e2/weather-vane-1424027.jpg',
+            'quiz_title': '두 번째 퀴즈',
+            'quiz_description': '흥미진진한 퀴즈입니다.',
+        },
+        {
+            'image_url': 'http://images.freeimages.com/images/previews/3e2/weather-vane-1424027.jpg',
+            'quiz_title': '세 번째 퀴즈',
+            'quiz_description': '풀어주세요',
+        },
+    ]
+
     ctx = {
+        'page_title': '트럼프 퀴즈에 오신 걸 환영합니다',
+        'quizzes': quizzes,
     }
     return render(request, 'toppage.html', ctx)
 
